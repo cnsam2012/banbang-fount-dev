036e96af18350334f406c3bb1375ab9e8a47416d
laocc <cnsam2012@foxmail.com>
2022-02-02 00:09:22 +0800

    fix: 解决‘更多’按钮不能按的问题
    删除‘popup’的索引

--- pages/star/star.vue
+++ pages/star/star.vue
@@ -1,6 +1,9 @@
 <template>
 	<view>
-		<noti-card :item="item"></noti-card>
+		<template v-for="(item, i) in items">
+			<noti-card :item="item"></noti-card>
+		</template>
+
 	</view>
 </template>
 
@@ -8,20 +11,27 @@
 	export default {
 		data() {
 			return {
-				item: {
+				items: [{
 					no: 20000,
 					title: '启动node服务！',
 					content: 'node服务没有启动！列表什么都没有！见 ../server/server.js',
 					date: "1984-01-01",
 					time: "20:15",
 					level: 3
-				}
+				}, {
+					no: 20001,
+					title: 'Buzhidao',
+					content: '第二条通知',
+					date: "1984-01-01",
+					time: "20:15",
+					level: 1
+				}]
 			};
 		},
 		methods: {
-			
+
 		},
-		
+
 	}
 </script>
 
