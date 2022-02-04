6691722f33385aacf4ad96dee679658794b54905
laocc <cnsam2012@foxmail.com>
2022-02-04 16:07:47 +0800

    perf: 优化搜索逻辑
    添加未读通知数量展示，添加未读通知折叠且默认展开；搜索框含关键词取消所有折叠

--- components/main-notiList/main-notiList.vue
+++ components/main-notiList/main-notiList.vue
@@ -3,82 +3,112 @@
 
 		<!-- 搜索框组件 -->
 		<!-- TODO：keywords数据默认为空字符串，但仍被编译器认为存在内容，原因未知 -->
-		<uni-search-bar @confirm="searchConfirm" bgColor="#eaeaea" placeholder="输入关键词" focus="false" radius="20"
-			v-model="keywords">
+		<uni-search-bar @confirm="searchConfirm" bgColor="#eaeaea" placeholder="输入关键词" radius="20" v-model="keywords"
+			cancelButton="auto">
 		</uni-search-bar>
 
-
-		<uni-collapse>
-			
-			<!-- 未读通知不被折叠 -->
-			<!-- 使用了uni-app卡片组件 -->
-			<!-- 约定 level >= 0 即为未读通知 -->
-			<!-- 渲染未读通知列表 -->
-			<view v-for="(item, i) in unreadNoti" :key="item.no">		
-				<noti-card :item='item' v-if="search(item,keywords)" :text="searchReturn(item,keywords)"></noti-card>
-			</view>
-
-			<!-- 折叠已读通知 -->
-			<uni-collapse-item title="全部已读" show-animation="true">
-				<!-- 使用了uni-app卡片组件 -->
-				<!-- 约定 level < 0 即为已读通知 --> 
-				<!-- 渲染已读通知列表 -->
-				<view v-for="(item, i) in readNoti" :key="item.no">
-					<noti-card :item='item' v-if="search(item,keywords)" :text="searchReturn(item,keywords)"></noti-card>
-					<!-- <noti-card :item='item'></noti-card> -->
+		<!-- 搜索框组件含关键词，渲染搜索结果 -->
+		<template v-if="keywords">
+			<uni-collapse>
+				<view v-for="(item, i) in allNoti" :key="item.no">
+					<noti-card :item='item' v-if="search(item,keywords)" :text="searchReturn(item,keywords)">
+					</noti-card>
 				</view>
-			</uni-collapse-item>
+			</uni-collapse>
+		</template>
 
-		</uni-collapse>
+		<!-- 无关键词，渲染全部 -->
+		<template v-else>
+			<uni-collapse v-model="openTab">
+				<!-- 未读通知默认不被折叠 -->
+				<!-- 使用了uni-app卡片组件 -->
+				<!-- 约定 level >= 0 即为未读通知 -->
+				<!-- 渲染未读通知列表 -->
+				<uni-collapse-item :title="unreadNotiCountMsg" show-animation="true" name="unreadNoti">
+					<view v-for="(item, i) in unreadNoti" :key="item.no">
+						<noti-card :item='item' v-if="search(item,keywords)" :text="searchReturn(item,keywords)">
+						</noti-card>
+					</view>
+				</uni-collapse-item>
+
+				<!-- 默认折叠已读通知 -->
+				<uni-collapse-item title="全部已读" show-animation="true" name="readNoti">
+					<!-- 使用了uni-app卡片组件 -->
+					<!-- 约定 level < 0 即为已读通知 -->
+					<!-- 渲染已读通知列表 -->
+					<view v-for="(item, i) in readNoti" :key="item.no">
+						<noti-card :item='item' v-if="search(item,keywords)" :text="searchReturn(item,keywords)">
+						</noti-card>
+						<!-- <noti-card :item='item'></noti-card> -->
+					</view>
+				</uni-collapse-item>
+			</uni-collapse>
+		</template>
+
 
 	</view>
 
 </template>
 
 <script>
-	import { mapState } from "vuex"
-	
+	import {
+		mapState
+	} from "vuex"
+
 	export default {
 		name: 'main-notiList',
 		data() {
 			return {
 				// 搜索框关键词绑定，默认值为空
 				keywords: "",
-				temp1: "abcdef",
-				tmep2: "gh",
-				temp3: "bcd",
-				temp4: "de"
+
+				// 计算属性  unreadNotiCountMsg: "全部未读（" + this.item.length + "）",
+
+				// 默认展开未读
+				openTab: ["unreadNoti"],
 			}
 		},
-		
+
 		// 生命周期函数，组件被挂载后调用
 		mounted() {
 			// console.log(this.$store.state.m_noti);
 			// console.log("*****************", this.temp1.match(this.temp2));
 			// console.log("*****************", this.temp1.match(this.temp3));
 			// console.log("*****************", this.temp1.match(this.temp4));
+			// console.log(this.unreadNoti.length);
 		},
-		
+
 		computed: {
 			// 分发 m_noti:noti.js 的 State
-			...mapState('m_noti', ['allNoti','unreadNoti','readNoti']),
+			...mapState('m_noti', ['allNoti', 'unreadNoti', 'readNoti']),
+
+			unreadNotiCountMsg() {
+				return "全部未读 (" + this.unreadNoti.length + ")";
+			}
 		},
 
 		methods: {
-			
+
+			// 仅关键词为空时，改变搜索状态
+			// altIsSeraching() {
+			// 	if(this.keywords == "") {
+			// 		this.isSearching = !this.isSearching;
+			// 	}
+			// },
+
 			//返回模糊查询结果
-			search(a,b) {
+			search(a, b) {
 				return a.content.match(b)
 			},
 			//返回查询并替换掉后的内容
-			searchReturn(a,b) {
-				return a.content.replace(new RegExp(b,"gm"),'<b style="color:black!important;">'+ b +'</b>')
+			searchReturn(a, b) {
+				return a.content.replace(new RegExp(b, "gm"), '<b style="color:black!important;">' + b + '</b>')
 			},
-			
+
 			// 响应搜索框的“搜索”动作
 			searchConfirm() {
 				// console.log(this.keywords);
-				
+
 				// 弹框提示输入的关键词
 				var msg = this.keywords;
 				wx.showToast({
