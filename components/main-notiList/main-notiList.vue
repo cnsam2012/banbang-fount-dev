768c4486c668ffae4c4d2338798977e1770fe488
laocc <cnsam2012@foxmail.com>
2022-02-07 22:41:35 +0800

    fix: 修复折叠面板在搜索时的显示异常
    小程序中不支持自动更新高度 ，需要手动调用resize 更新组件高度

--- components/main-notiList/main-notiList.vue
+++ components/main-notiList/main-notiList.vue
@@ -4,46 +4,47 @@
 		<!-- 搜索框组件 -->
 		<!-- TODO：keywords数据默认为空字符串，但仍被编译器认为存在内容，原因未知 -->
 		<uni-search-bar @confirm="searchConfirm" bgColor="#eaeaea" placeholder="输入关键词" radius="20" v-model="keywords"
-			cancelButton="auto">
+			cancelButton="auto" @input="refleshColl">
 		</uni-search-bar>
 
-		<!-- 搜索框组件含关键词，渲染搜索结果 -->
-		<template v-if="keywords">
+		<!-- 搜索框组件含关键词，渲染搜索结果 - 弃用：现折叠面板能根据内容多少动态调整高度 -->
+		<!-- <template v-if="keywords">
 			<uni-collapse>
 				<view v-for="(item, i) in allNoti" :key="item.no">
 					<noti-card :item='item' v-if="search(item,keywords)" :text="searchReturn(item,keywords)">
 					</noti-card>
 				</view>
 			</uni-collapse>
-		</template>
+		</template> -->
 
-		<!-- 无关键词，渲染全部 -->
-		<template v-else>
-			<uni-collapse v-model="openTab">
-				<!-- 未读通知默认不被折叠 -->
+		<!-- 无关键词，渲染全部 - 弃用：现折叠面板能根据内容多少动态调整高度 -->
+		<!-- <template v-else> -->
+		<!-- 无论有无关键词，均渲染全部折叠面板 -->
+		<uni-collapse v-model="openTab">
+			<!-- 未读通知默认不被折叠 -->
+			<!-- 使用了uni-app卡片组件 -->
+			<!-- 约定 level >= 0 即为未读通知 -->
+			<!-- 渲染未读通知列表 -->
+			<uni-collapse-item :title="unreadNotiCountMsg" show-animation="true" name="unreadNoti" ref="unreadNoti">
+				<view v-for="(item, i) in unreadNoti" :key="item.no">
+					<noti-card :item='item' v-if="search(item,keywords)" :text="searchReturn(item,keywords)">
+					</noti-card>
+				</view>
+			</uni-collapse-item>
+
+			<!-- 默认折叠已读通知 -->
+			<uni-collapse-item title="全部已读" show-animation="true" name="readNoti" ref="readNoti">
 				<!-- 使用了uni-app卡片组件 -->
-				<!-- 约定 level >= 0 即为未读通知 -->
-				<!-- 渲染未读通知列表 -->
-				<uni-collapse-item :title="unreadNotiCountMsg" show-animation="true" name="unreadNoti">
-					<view v-for="(item, i) in unreadNoti" :key="item.no">
-						<noti-card :item='item' v-if="search(item,keywords)" :text="searchReturn(item,keywords)">
-						</noti-card>
-					</view>
-				</uni-collapse-item>
-
-				<!-- 默认折叠已读通知 -->
-				<uni-collapse-item title="全部已读" show-animation="true" name="readNoti">
-					<!-- 使用了uni-app卡片组件 -->
-					<!-- 约定 level < 0 即为已读通知 -->
-					<!-- 渲染已读通知列表 -->
-					<view v-for="(item, i) in readNoti" :key="item.no">
-						<noti-card :item='item' v-if="search(item,keywords)" :text="searchReturn(item,keywords)">
-						</noti-card>
-						<!-- <noti-card :item='item'></noti-card> -->
-					</view>
-				</uni-collapse-item>
-			</uni-collapse>
-		</template>
+				<!-- 约定 level < 0 即为已读通知 -->
+				<!-- 渲染已读通知列表 -->
+				<view v-for="(item, i) in readNoti" :key="item.no">
+					<noti-card :item='item' v-if="search(item,keywords)" :text="searchReturn(item,keywords)">
+					</noti-card>
+					<!-- <noti-card :item='item'></noti-card> -->
+				</view>
+			</uni-collapse-item>
+		</uni-collapse>
+		<!-- </template> -->
 
 
 	</view>
@@ -117,6 +118,14 @@
 					icon: 'success',
 					duration: 1500
 				})
+			},
+
+			refleshColl() {
+				// console.log(this.$refs.unreadNoti.collapse);
+				this.$nextTick(function(){
+					this.$refs.unreadNoti.collapse.resize();
+					this.$refs.readNoti.collapse.resize();
+				});
 			}
 		}
 	}
